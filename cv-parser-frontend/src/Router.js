3302ab7c768febdb74515afc27bbc9428987f554
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2023-03-01 13:47:08 +0800

    installed icons-material and edited the layout for ecport.

--- cv-parser-frontend/src/Router.js
+++ cv-parser-frontend/src/Router.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Login from './Screens/Login';
-<<<<<<< HEAD
 import FileUploader from './Screens/Upload';
 import Create from './Screens/Create';
 import { Route, Routes } from 'react-router-dom'
@@ -8,9 +7,6 @@ import ListParseItems from './Screens/ListParseItems';
 import Dashboard from './Screens/Dashboard'
 import NavBar from './Component/Navbar';
 import ListProfileCreated from './Screens/ListProfileCreated';
-=======
-import {Route, Routes } from 'react-router-dom'
->>>>>>> 7769897 (Created basic template for file export route:)
 import FileExport from './Screens/FileExport';
 
 
