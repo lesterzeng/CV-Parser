b502adb3fb3905eae74875037a6274ebf5a887e2
Eilumial <121159565+Eilumial@users.noreply.github.com>
2023-03-03 16:13:44 +0800

    Implemented file size checks and handling

--- cv-parser-frontend/src/Screens/Upload.jsx
+++ cv-parser-frontend/src/Screens/Upload.jsx
@@ -13,10 +13,13 @@ function FileUploader() {
       successCount: 0,
    };
    const [selectedFiles, setSelectedFiles] = useState([]);
+   const [uploadMode, setUploadMode] = useState(true);
    const [loading, setLoading] = useState(false);
    const [success, setSuccess] = useState(false);
    const [error, setError] = useState(false);
+   const [sizeError, setSizeError] = useState(false);
    const [errorList, setErrorList] = useState(dummyError);
+   const [sizeErrorList, setSizeErrorList] = useState(dummyError);
 
    const fileInputRef = useRef(null);
 
@@ -40,15 +43,127 @@ function FileUploader() {
 
    const handleSubmit = async (event) => {
       event.preventDefault();
+
+      // Check if no files are selected
+      if (selectedFiles.length === 0) {
+         alert("Please select one or more files to upload.");
+         return;
+      }
+
       setLoading(true);
       setSuccess(false);
-
+      const maxFileSize = 5 * 1024 * 1024; // 5MB
+      const maxTotalFileSize = 10 * 1024 * 1024; //100MB
+      let totalFileSize = 0;
+      let invalidFiles = [];
       const formData = new FormData();
+      // for (const file of selectedFiles) {
+      //    formData.append("files", file);
+      // }
+
       for (const file of selectedFiles) {
-         formData.append("files", file);
+         if (file.size > maxFileSize) {
+            invalidFiles.push(file);
+         } else {
+            formData.append("files", file);
+            totalFileSize += file.size;
+         }
       }
+
+      if (invalidFiles.length > 0) {
+         // alert(
+         //    `The following files exceed the maximum file size limit of ${
+         //       maxFileSize / 1024 / 1024
+         //    } MB: ${invalidFiles.join(", ")}`
+         // );
+         // setSizeErrorList(dummyError);
+         // console.log(invalidFiles);
+         // const errorMessage = `The following files exceed the maximum file size limit of ${
+         //    maxFileSize / 1024 / 1024
+         // } MB: ${invalidFiles.join(", ")}`;
+         // const invalidFileErrors = invalidFiles.map((fileName) => ({
+         //    fileName,
+         //    errorMessage,
+         // }));
+         // setSizeErrorList({
+         //    errors: [...sizeErrorList.errors, ],
+         //    failCount: sizeErrorList.failCount + invalidFiles.length,
+         //    successCount: sizeErrorList.successCount,
+         // });
+
+         const newErrors = [];
+         for (const file of invalidFiles) {
+            const fileSizeInMB = (file.size / (1024 * 1024)).toFixed(2);
+            const maxFileSizeInMB = (maxFileSize / (1024 * 1024)).toFixed(0);
+            newErrors.push({
+               fileName: file.name,
+               errorMessage: `This file's size is ${fileSizeInMB} MB, over max file size of ${maxFileSizeInMB} MB`,
+            });
+         }
+         const newSizeErrorList = {
+            errors: newErrors,
+            failCount: newErrors.length,
+            successCount: 0,
+         };
+         setSizeErrorList(newSizeErrorList);
+
+         console.log(sizeErrorList);
+         setSizeError(true);
+         setError(false);
+         setLoading(false);
+         setSelectedFiles([]);
+         return;
+      }
+
+      if (totalFileSize > maxTotalFileSize) {
+         // alert(
+         //    `The total file size exceeds the maximum file size limit of ${
+         //       maxTotalFileSize / 1024 / 1024
+         //    } MB.`
+         // );
+         const totalFileSizeInMB = (totalFileSize / (1024 * 1024)).toFixed(2);
+         const maxTotalFileSizeInMB = (
+            maxTotalFileSize /
+            (1024 * 1024)
+         ).toFixed(0);
+         const newErrors = [
+            `Total batch file size is ${totalFileSizeInMB}MB and exceeds max file size of ${maxTotalFileSizeInMB}MB`,
+         ];
+         // for (const file of invalidFiles) {
+         //    const fileSizeInMB = (file.size / (1024 * 1024)).toFixed(2);
+         //    const maxFileSizeInMB = (maxFileSize / (1024 * 1024)).toFixed(0);
+         //    newErrors.push({
+         //       fileName: file.name,
+         //       errorMessage: `This file's size is ${fileSizeInMB} MB, over max file size of ${maxFileSizeInMB} MB`,
+         //    });
+         // }
+         const newSizeErrorList = {
+            errors: [
+               {
+                  fileName: "Current Upload Batch",
+                  errorMessage: newErrors,
+               },
+            ],
+            failCount: newErrors.length,
+            successCount: 0,
+         };
+         setSizeErrorList(newSizeErrorList);
+
+         console.log(sizeErrorList);
+         setSizeError(true);
+         setError(false);
+         setLoading(false);
+         setSelectedFiles([]);
+         return;
+      }
+
+      // const headers = {
+      //    Authorization: `Bearer ${Cookies.get("jwt")}`,
+      //    "Content-Type": "application/json",
+      // };
       fetch("http://localhost:8080/api/upload/up", {
          method: "POST",
+         // headers: headers,
          body: formData,
       })
          .then((response) => {
@@ -56,12 +171,15 @@ function FileUploader() {
                setSuccess(true);
                setError(false);
                setErrorList(dummyError);
+               setUploadMode(false);
             } else if (response.status === 400) {
                // Handle bad request
                response.json().then((data) => {
                   console.log(data); // Log the error message returned from the server
                   setErrorList(data);
                   setError(true);
+                  setSizeError(false);
+                  setUploadMode(false);
                });
             } else {
                // Handle other errors
@@ -107,50 +225,86 @@ function FileUploader() {
          setSelectedFiles([]);
       }
    };
-
+   const handleUploadMode = () => {
+      setUploadMode(true);
+      setError(false);
+      setSuccess(false);
+      setErrorList(dummyError);
+      setSizeErrorList(dummyError);
+      setSizeError(false);
+      setLoading(false);
+      setSelectedFiles([]);
+   };
    return (
       <form onSubmit={handleSubmit}>
-         <div
-            className="file-uploader"
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            onClick={() => fileInputRef.current.click()}
-         >
-            <input
-               type="file"
-               multiple
-               ref={fileInputRef}
-               onChange={handleFileSelect}
-               style={{ display: "none" }}
-            />
-            {/* <p>Drag and drop files here, or click to select files</p> */}
+         {uploadMode && (
+            <div>
+               <div
+                  className="file-uploader"
+                  onDrop={handleDrop}
+                  onDragOver={handleDragOver}
+                  onClick={() => fileInputRef.current.click()}
+               >
+                  <input
+                     type="file"
+                     multiple
+                     ref={fileInputRef}
+                     onChange={handleFileSelect}
+                     style={{ display: "none" }}
+                  />
+                  {/* <p>Drag and drop files here, or click to select files</p> */}
 
-            {loading ? (
-               <div className="spinner" />
-            ) : (
-               <p>Drag and drop files here, or click to select files</p>
-            )}
+                  {loading ? (
+                     <div className="spinner" />
+                  ) : (
+                     <p>Drag and drop files here, or click to select files</p>
+                  )}
 
-            {selectedFiles.length > 0 && (
-               <p>Selected {selectedFiles.length} file(s)</p>
-            )}
-            {success && <p>Upload successful!</p>}
-         </div>
-         <button type="submit" disabled={loading}>
-            {loading ? "Uploading..." : "Upload"}
-         </button>
-         <button type="button" onClick={handleClear}>
-            Clear
-         </button>
+                  {selectedFiles.length > 0 && (
+                     <p>Selected {selectedFiles.length} file(s)</p>
+                  )}
+               </div>
+               <button type="submit" disabled={loading}>
+                  {loading ? "Uploading..." : "Upload"}
+               </button>
+               <button type="button" onClick={handleClear}>
+                  Clear
+               </button>
+            </div>
+         )}
+         {success && <p>Upload successful!</p>}
          {error && (
             <div>
-               <p>Upload successful: {errorList.successCount}</p>
-               <p>Upload failed: {errorList.failCount}</p>
+               <p>Total Upload Successful: {errorList.successCount}</p>
+               <p>Upload Failed (in current batch): {errorList.failCount}</p>
                {errorList.errors.map((error) => (
                   <p>
                      {error.fileName} {error.errorMessage}
                   </p>
                ))}
+            </div>
+         )}
+         {sizeError && (
+            <div>
+               {sizeErrorList.errors.map((error) => (
+                  <p>
+                     {error.fileName} {error.errorMessage}
+                  </p>
+               ))}
+            </div>
+         )}
+         {!uploadMode && (
+            <div>
+               <button
+                  type="button"
+                  onClick={() => {
+                     handleUploadMode();
+                  }}
+               >
+                  Add More Files
+               </button>
+               <button type="button">Quick Create</button>
+               <button type="button">Parse</button>
             </div>
          )}
       </form>
