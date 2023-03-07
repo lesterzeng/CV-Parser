23aebb2770055a1b27018f2ad8cf4d4a119dfa83
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2023-03-07 16:15:38 +0800

    resolved conflicts.

--- cv-parser-frontend/src/Router.js
+++ cv-parser-frontend/src/Router.js
@@ -3,13 +3,10 @@ import Login from './Screens/Login';
 import FileUploader from './Screens/Upload';
 import Create from './Screens/Create';
 import { Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
 import ListParseItems from './Screens/ListParseItems';
 import Dashboard from './Screens/Dashboard'
 import NavBar from './Component/Navbar';
 import ListProfileCreated from './Screens/ListProfileCreated';
-=======
->>>>>>> 5091d9bbe7085c1306daf834f09d8dcb9ea2fa48
 import FileExport from './Screens/FileExport';
 
 
@@ -17,7 +14,6 @@ function Router() {
   return (
     <Routes>
       <Route path="/login" element={<Login />}></Route>
-<<<<<<< HEAD
       <Route path="/dashboard" element={<Dashboard />}></Route>
       <Route path="/cvparse/cand" element={<ListParseItems />}></Route>
       <Route path="/cvparse/cand/profile" element={<ListProfileCreated />}></Route>
@@ -25,11 +21,6 @@ function Router() {
       <Route path="/create" element={<Create />}></Route>
       <Route path="/export" element={<FileExport />}></Route>
       <Route path="/export" element={<FileExport />}></Route>
-=======
-      <Route path="/upload" element={<FileUploader />}></Route>
-      <Route path="/create" element={<Create />}></Route>
-      <Route path="/export" element={<FileExport />}></Route>
->>>>>>> 5091d9bbe7085c1306daf834f09d8dcb9ea2fa48
     </Routes>
   );
 }
